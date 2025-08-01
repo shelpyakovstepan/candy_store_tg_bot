7d2784445c0e3962c39d3f85299140e32f68c7d0
shelpyakovstepan <step3210shelpyakov@gmail.com>
2025-08-01 18:27:20 +0300

    Сделал приветственное сообщение

--- bot/main.py
+++ bot/main.py
@@ -5,6 +5,8 @@ import asyncio
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart
+from aiogram.types import Message
 
 # FIRSTPARTY
 from bot.config import settings
@@ -13,26 +15,19 @@ TOKEN = settings.BOT_TOKEN
 
 
 dp = Dispatcher()
-
-
-# @dp.message(CommandStart())
-# async def command_start_handler(message: Message) -> None:
-#    await message.answer(f"Привет, {html.bold(message.from_user.full_name)}!")
-#
-#
-# @dp.message()
-# async def echo_handler(message: Message) -> None:
-#    try:
-#        await message.send_copy(chat_id=message.chat.id)
-#    except TypeError:
-#        await message.answer("Nice try!")
-#
-
 bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 
 
+@dp.message(CommandStart())
+async def command_start_handler(message: Message) -> None:
+    await message.answer(
+        "Привет!\n"
+        "Это бот кондитерского магазина, который будет уведомлять вас о заказах и изменениях статуса их готовности!\n\n"
+        "Удачных заказов!"
+    )
+
+
 async def main() -> None:
-    # And the run events dispatching
     await dp.start_polling(bot)
     print("Bot started")
 
